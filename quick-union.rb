bad458f28d1489634f764691f75a0a05bd51d1af
Ding Yu <felixding@gmail.com>
2020-12-16 21:59:36 +0900

    add quick-union

---
+++ quick-union.rb
@@ -0,0 +1,45 @@
+class QuickUnion
+  attr :items, true
+
+  def initialize n
+    @items = Array.new(n) {|i| i}
+  end
+
+  def connected? p, q
+    root(p) == root(q)
+  end
+
+  def union p, q
+    @items[root(p)] = root(q)
+
+    puts
+    puts "Connected #{p} with #{q}"
+    puts "@items: #{@items}"
+  end
+
+  private
+
+  def root i
+    @items[i] == i ? i : root(@items[i])
+  end
+end
+
+operations = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8],
+  [7, 9],
+  [2, 8],
+  [0, 5],
+  [1, 9]
+]
+
+uf = QuickUnion.new 10
+
+operations.each do |operation|
+  p, q = operation
+
+  #puts "p: #{p}, q: #{q}"
+  uf.union p, q
+end
