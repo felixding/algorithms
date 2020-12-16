576018c39a4120b61028b6901bbc8eb79cc0ab79
Ding Yu <felixding@gmail.com>
2020-12-16 20:57:14 +0900

    add dynamic-connectivity

---
+++ dynamic-connectivity.rb
@@ -0,0 +1,48 @@
+class UnionFind
+  attr :items, true
+
+  def initialize n
+    @items = Array.new(n) {|i| i}
+  end
+
+  def connected? p, q
+    @items[p] == @items[q]
+  end
+
+  def union p, q
+    pid = @items[p]
+
+    @items.each_index do |i|
+      #puts
+      #puts "i: #{i}, @items[i]: #{@items[i]}, @items[p]: #{@items[p]}, (@items[i] == @items[p]): #{(@items[i] == @items[p])}"
+
+      if @items[i] == pid
+        @items[i] = @items[q]
+        puts "Connected #{p} with #{q}"
+      end
+    end
+
+    puts
+    puts "@items: #{@items}"
+  end
+end
+
+operations = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8],
+  [7, 9],
+  [2, 8],
+  [0, 5],
+  [1, 9]
+]
+
+uf = UnionFind.new 10
+
+operations.each do |operation|
+  p, q = operation
+
+  #puts "p: #{p}, q: #{q}"
+  uf.union p, q
+end
