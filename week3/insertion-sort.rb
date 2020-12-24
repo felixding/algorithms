f4e7628991bbd67184d6589895bf223ecf4de4a7
Ding Yu <felixding@gmail.com>
2020-12-24 14:48:21 +0900

    add insertion-sort.rb

---
+++ week3/insertion-sort.rb
@@ -0,0 +1,20 @@
+def insertion_sort array
+  array.size.times do |i|
+    while i > 0
+      array[i - 1] > array[i] ? (array[i - 1], array[i] = array[i], array[i - 1]) : break
+
+      i -= 1
+    end
+  end
+
+  array
+end
+
+#array_to_sort = %w(dd eb ea ae ac ab bd ca).to_a.shuffle
+array_to_sort = (0..9).to_a.shuffle
+
+puts "Array to sort: #{array_to_sort}"
+
+sorted = insertion_sort array_to_sort
+
+puts "Array sorted: #{sorted}"
